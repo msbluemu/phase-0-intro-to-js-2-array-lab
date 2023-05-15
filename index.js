7d85a625ed7007974ccfbec77b810ee024cf4407
Qianyun Mu <msbluemu@gmail.com>
2023-05-15 16:23:15 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(){
+    const newCat = [...cats, "Broom"];
+    return newCat
+}
+
+function prependCat(){
+    const newCat = ["Arnold",...cats];
+    return newCat
+}
+
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
